97c7e5e5fe139cfb5b9d91b2064d7722b7188cab
p2acoo <78920246+p2acoo@users.noreply.github.com>
2022-06-14 09:19:51 +0200

    send into sftp server, more config, some bug fix

--- backup.sh
+++ backup.sh
@@ -1,89 +1,45 @@
 #!/bin/bash
 # backup.sh
-. /home/pi/ShellBackup/backup.config
-type read
-NOW=`date '+%F'`;
+
+# get config values
+. /home/theotruvelott/backup.config
+NOW=`date '+%F'`
+
 mkdir "${backupfile}_${NOW}"
 mkdir "${backupfile}_${NOW}/sql"
 mkdir "${backupfile}_${NOW}/logs"
+mkdir "${backupfile}_${NOW}/vh"
 
-echo "Lien du dossier www à copier : $wwwfile"
-echo "Lien du dossier backupfile : $backupfile"
-echo "Existance d'un db : $aredb"
-echo "Système de gestion de base de données : $SGBD"
-echo "Database Name : $dbname"
-echo "DB Host : $dbhost"
-echo "DB User : $dbuser"
-echo "DB Password : $dbpsswd"
-echo "Existance de virtuals hosts : $arevh"
-echo "Lien du dossier virtual host : $vhfile"
-echo ""
-
+# func for logs
 logs() {
-    echo "$(date '+%Y-%m-%d %H:%M:%S') | $1" >> "${backupfile}_${NOW}/logs/logs_${NOW}.txt"
-}
-# Ititialization
 
-mainmenu() {
-    echo "Voulez vous lancez la procédure ? "
-    read -n 1 -p "Y/N : " input
-    if  [ "$input" = "Y" ]; then
-        clear
-        logs "------------------------"
-        logs "-- logs du ${NOW} --"
-        logs "------------------------"
-        logs ""
-        backup
-        elif [ "$input" = "N" ]; then
-        rm -r "${backupfile}_${NOW}"
-        echo "bye"
-        exit
-    else
-        rm -r "${backupfile}_${NOW}"
-        echo "input invalid"
-        exit
-    fi
-    
+    echo "$(date '+%Y-%m-%d %H:%M:%S') | $1" >> "${outputlogs}/logs_$NOW.log"
 }
+#  launching backup
 
 backup() {
-    cd "/var/www/"
-    echo "DÉBUT DE LA COPIE DES FICHIER WWW"
+    logs "------------------------------------------"
+    logs "-- logs du $(date '+%Y-%m-%d %H:%M:%S') --"
+    logs "------------------------------------------"
+    logs ""
+    # Backup of the www files
     logs "DÉBUT DE LA COPIE DES FICHIER WWW"
-    case $? in
-        0) cp -R "${wwwfile}" -t "${backupfile}_${NOW}"
+   
+     case $arewww in
+        Y) 
+            logs "DÉBUT DE LA COPIE DES FICHIER www"
             case $? in
-                0) echo "COPIE DU FICHIER WWW EFFECTUÉ"
-                    logs "COPIE DU FICHIER WWW EFFECTUÉ"
-                ;;
-                1) echo "ERREUR DANS LA COPIE DU FICHIER WWW"
-                    logs "ERREUR DANS LA COPIE DU FICHIER WWW"
-                    exit
-                ;;
-                *) echo "ERREUR SYSTEME"
-                    logs "ERREUR SYSTEM"
-                    exit
-                ;;
-        esac ;;
-        1) echo "ERREUR ACCES AU FICHIER WWW"
-            logs "ERREUR ACCES AU FICHIER WWW"
-        exit ;;
-    esac
-    case $arevh in
-        Y) cd "/etc/apache2/sites-available"
-            echo "DÉBUT DE LA COPIE DES FICHIER VH"
-            case $? in
-                0) cp -R "${$vhfile}" -t "${backupfile}_${NOW}"
+                0) cp -R "${wwwfile}" -t "${backupfile}_${NOW}"
                     case $? in
-                        0) echo "COPIE DU DOSSIER VIRTUALS HOSTS EFFECTUÉ"
-                            logs "COPIE DU DOSSIER VIRTUALS HOSTS EFFECTUÉ"
+                        0) 
+                            logs "COPIE DU DOSSIER WWW EFFECTUÉ"
                         ;;
-                        1) echo "ERREUR DANS LA COPIE DES VIRTUALS HOSTS"
-                            logs "ERREUR DANS LA COPIE DES VIRTUALS HOSTS"
+                        1)
+                            logs "ERREUR DANS LA COPIE DU DOSSIER WWW"
                             exit
                         ;;
-                        *) echo "ERREUR SYSTEME"
-                            logs "ERREUR SYSTEME"
+                        *) 
+                            logs "ERREUR SYSTEME (cp) ! code d'erreur : $?"
                             exit
                         ;;
                 esac ;;
@@ -93,59 +49,144 @@ backup() {
                 exit;;
             esac
     esac
+
+    # backup of virtualhost
+    case $arevh in
+        Y) 
+            logs "DÉBUT DE LA COPIE DES FICHIER VH"
+            case $? in
+                0) cp -R "${vhfile}" -t "${backupfile}_${NOW}/vh"
+                    case $? in
+                        0) 
+                            logs "COPIE DU DOSSIER VIRTUALS HOSTS EFFECTUÉ"
+                        ;;
+                        1)
+                            logs "ERREUR DANS LA COPIE DES VIRTUALS HOSTS"
+                            exit
+                        ;;
+                        *) 
+                            logs "ERREUR SYSTEME (cp) ! code d'erreur : $?"
+                            exit
+                        ;;
+                esac ;;
+                N)
+                ;;
+                *) echo "ERREUR CONFIG FILE"
+                exit;;
+            esac
+    esac
+
+    # backup of database
     case $aredb in
         Y) case $SGBD in
                 MYSQL)
-                    echo "DEBUT DE LA COPIE DE LA DB MYSQL"
+                   
                     logs "DEBUT DE LA COPIE DE LA DB MYSQL"
                     mysqldump -h $dbhost --user="$dbuser" --password="$dbpsswd" $dbname > ${backupfile}_${NOW}/sql/dump_bdd_mysql.sql
                     if [ $? -eq 0 ]
                     then
-                        echo "COPIE DE LA DB EFFECTUÉ"
+                      
                         logs "COPIE DE LA DB EFFECTUÉ"
                     else
-                        echo "ERREUR DE LA COPIE DE LA DB"
-                        logs "ERREUR DE LA COPIE DE LA DB"
+                       
+                        logs "ERREUR DE LA COPIE DE LA DB ! code d'erreur : $?"
                     fi
                 ;;
                 POSTGRESQL)
-                    echo "DEBUT DE LA COPIE DE LA DB POSTGRESQL"
-                    lgos "DEBUT DE LA COPIE DE LA DB POSTGRESQL"
+                    
+                    logs "DEBUT DE LA COPIE DE LA DB POSTGRESQL"
                     pg_dump -h $dbhost -u $dbuser -w $dbpsswd -d $dbname > ${backupfile}_${NOW}/sql/dump_bdd_Postgre.sql
                     if [ $? -eq 0 ]
                     then
-                        echo "COPIE DE LA DB EFFECTUÉ"
                         logs "COPIE DE LA DB EFFECTUÉ"
                     else
-                        echo "ERREUR DE LA COPIE DE LA DB"
-                        logs "ERREUR DE LA COPIE DE LA DB"
+                        logs "ERREUR DE LA COPIE DE LA DB ! code d'erreur : $?"
                     fi
                 ;;
                 SQLSERVER)
-                    echo "DEBUT DE LA COPIE DE LA DB SQL SERVER"
                     logs "DEBUT DE LA COPIE DE LA DB SQL SERVER"
                     sqlcmd -S $dbhost -U $dbuser -P $dbpsswd -Q "BACKUP DATABASE [$dbname] TO DISK = N'${backupfile}_${NOW}/sql/dump_bdd_SQLserv.sql"
                     if [ $? -eq 0 ]
                     then
-                        echo "COPIE DE LA DB EFFECTUÉ"
                         logs "COPIE DE LA DB EFFECTUÉ"
                     else
-                        echo "ERREUR DE LA COPIE DE LA DB"
-                        logs "ERREUR DE LA COPIE DE LA DB"
+                        logs "ERREUR DE LA COPIE DE LA DB ! code d'erreur : $?"
                     fi
                 ;;
                 N)
                 ;;
-                *) echo "SGBD NON SUPPORTÉ"
-                    echo "ERREUR DANS LE CONFIG FILE"
+                *) 
                     logs "SGBD NON SUPPORTÉ"
                     logs "ERREUR DANS LE CONFIG FILE"
                 ;;
             esac
     esac
-    
-}
-mainmenu
 
-echo "FIN DU SCRIPT"
-logs "FIN DU SCRIPT"
+    # backup of logs
+    case $arelogs in
+        Y) 
+            logs "DÉBUT DE LA COPIE DES LOGS"
+            case $? in
+                0) cp -R "${logfile}" -t "${backupfile}_${NOW}/logs"
+                    case $? in
+                        0) 
+                            logs "COPIE DES LOGS EFFECTUÉ"
+                        ;;
+                        1) 
+                            logs "ERREUR DANS LA COPIE DES LOGS"
+                            exit
+                        ;;
+                        *) 
+                            logs "ERREUR SYSTEM (cp) ! code d'erreur : $?"
+                            exit
+                        ;;
+                esac ;;
+                N)
+                ;;
+                *) echo "ERREUR CONFIG FILE"
+                exit;;
+            esac
+    esac
+
+    # zip files
+    echo ${backupfile}_${NOW}
+    cd ${backupfile}_${NOW}
+    zip -q -r ${backupfile}_${NOW}.zip ./*
+    case $? in
+        0) logs "ZIP EFFECTUÉ"
+        ;;
+        1) logs "ERREUR DANS LE ZIP"
+        ;;
+        *) logs "ERREUR SYSTEME (zip) ! code d'erreur : $?"
+        ;;
+    esac
+    # remove not needed files
+	rm -R ${backupfile}_${NOW}
+    case $? in
+        0) logs "SUPPRESSION DU DOSSIER ${backupfile}_${NOW} EFFECTUÉ"
+        ;;
+        1) logs "ERREUR DANS LA SUPPRESSION DU DOSSIER ${backupfile}_${NOW}"
+        ;;
+        *) logs "ERREUR SYSTEME (rm) ! code d'erreur : $?"
+        
+    esac
+    # send files to sftp server 
+	sftp ${sftpuser}@{sftpip} <<< $"put ${backupfile}_${NOW}.zip"
+    case $? in
+        0) logs "ENVOI DU FICHIER ZIP EFFECTUÉ"
+        ;;
+        1) logs "ERREUR DANS L\'ENVOI DU FICHIER ZIP"
+        ;;
+        *) logs "ERREUR SYSTEME (sftp) ! code d'erreur : $?"
+        ;;
+    esac
+
+
+
+    logs "FIN DU SCRIPT"
+
+
+	exit	
+}
+backup
+
